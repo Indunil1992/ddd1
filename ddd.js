34a3f5af25cda4d5b2eb2e977442bda1ba5de6cc
Indunil1992 <indunil@adroitlogic.com>
2021-07-06 14:58:07 +0530

    change to Frnckt, add env variable, change edit msg

--- ddd.js
+++ ddd.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk');
 const cognito_idp = new AWS.CognitoIdentityServiceProvider();
 
 exports.handler = async (event) => {
+    debugger;
     try {
         let data = await cognito_idp.listUsers({
             UserPoolId: process.env.UserPoolId_cognitoinduJuly6L21,
